882f26645bcdd1918a489734449c3ec30bccdeb7
josuerushanika <josuerushanika@gmail.com>
2023-07-11 05:27:55 +0200

    Update redux

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,3 +1,10 @@
 class GreetingsController < ApplicationController
-    
+    def index
+        @greeting = Greeting.order('RANDOM()').first
+
+        respond_to do |format|
+            format.html
+            format.json { render json: @greeting }
+        end     
+    end
 end
---
+++ app/javascript/redux/Slice/greetingSlice.js
@@ -0,0 +1,43 @@
+import axios from  "axios"
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+    greetingstore: [],
+    status: 'idle',
+    error: null
+};
+
+const BASE_URL = "http://localhost:3000/greetings";
+
+export const fetchGreeting = createAsyncThunk(
+    "greetings/fetchgreetings",
+    async () => {
+        const response = await axios.get(BASE_URL);
+        return response.data
+    }
+);
+
+const greetingSlice = createSlice({
+    name: "greetings",
+    initialState,
+    reducers: {},
+
+    extraReducers: (builder) => {
+        builder.addCase(fetchGreeting.pending, (state) => {
+            state.status = 'Loading';
+        });
+
+        builder.addCase(fetchGreeting.fulfilled, (state, action) => {
+            state.status = 'succeded';
+            state.greetingstore = state.greetingstore.concat(action.payload)
+        });
+
+        builder.addCase(fetchGreeting.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message
+        });
+
+    }
+})
+
+export default greetingSlice.reducer;
