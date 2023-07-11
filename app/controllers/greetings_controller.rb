45ef3ba207f7cf8b5600a5a2c7e48dd77a838a95
josuerushanika <josuerushanika@gmail.com>
2023-07-11 12:13:37 +0200

    Fix linters

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class GreetingsController < ApplicationController
   def index
     @greeting = Greeting.order('RANDOM()').first
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This is a description of the ApplicationHelper module.
 module ApplicationHelper
 end
